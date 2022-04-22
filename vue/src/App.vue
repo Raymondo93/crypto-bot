555b7a87a489653a50dc54a88a93bcdb4a3cb4bc
Raymond <raymond.debruine@gmail.com>
2022-04-22 04:24:15 +0200

    added vue initial, need to add store management

---
+++ vue/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1>Header text</h1>
+  </header>
+  <RouterView />
+</template>
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+const helloWorldMessage = "Hello World, crypto nerds!"
+</script>
+
+<template>
+  <main>
+    <h1>{{ helloWorldMessage }}</h1>
+    <h3>dit is een test</h3>
+  </main>
+</template>
